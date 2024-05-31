9b4b771cba69af71f7bb1bb81398fc5c22adc2c8
HrxSrv <harshitsrv2004@gmail.com>
2024-05-31 15:34:54 +0530

    second commit

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -51,10 +51,10 @@ const Footer = () => {
       <div className="bottom-bar">
         <div className="bottom-bar-content">
           <span className="text">
-            Developed By Harshit Srivastava
+            Designed By Vishrut Sagar
           </span>
           <span className="text">
-            Designed By Vishrut Sagar
+            Developed By Harshit Srivastava
           </span>
           <img src={Payment} />
         </div>
