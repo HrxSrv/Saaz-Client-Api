485ab6dafc7ce8676a66fb25cc5e1e9415b93b52
HrxSrv <harshitsrv2004@gmail.com>
2024-06-05 20:56:42 +0530

    Added PopUp tiles

--- client/src/components/Home/Drawer/Drawer.jsx
+++ client/src/components/Home/Drawer/Drawer.jsx
@@ -1,44 +1,200 @@
-import React, { useState, forwardRef } from 'react';
-import { Dialog, DialogActions, DialogContent, DialogTitle, Button, Slide } from '@mui/material';
-
+import React, { useState, forwardRef } from "react";
+import {
+  Dialog,
+  DialogContent,
+  Typography,
+  Slide,
+  IconButton,
+} from "@mui/material";
+import { styled } from "@mui/system";
+import CloseIcon from "@mui/icons-material/Close";
+import arrow from "../../../assets/arrow3x.png"; // Import the arrow image
+import Mic from "../../../assets/Mic.png"
 const Transition = forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
 });
+const TileContainer = styled("div")(({ theme }) => ({
+  display: "grid",
+  gridTemplateAreas: `
+    'tile1 tile1 tile1 tile1 tile1 tile1 tile1 tile1 tile2 tile2'
+    'tile1 tile1 tile1 tile1 tile1 tile1 tile1 tile1 tile3 tile4'
+  `,
+  gridGap: "8px",
+  width: "100%",
+  height: "100%",
+  background: "#181818",
+  [theme.breakpoints.up("md")]: {
+    width: "60vw",
+    gridTemplateAreas: `
+    'tile1 tile1 tile1 tile1 tile1 tile1  tile2 tile2'
+    'tile1 tile1 tile1 tile1 tile1 tile1  tile3 tile4'
+  `,
+  },
+}));
+const Tile = styled("div")(({ area }) => ({
+  background: "black",
+  gridArea: area,
+  border: "1px solid black",
+  padding: "16px",
+  boxSizing: "border-box",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  position: "relative",
+  opacity: "80%",
+  transition: "ease-out",
+  borderRadius: "1vw",
+  "&:hover": {
+    opacity: "100%",
+    border: "1px solid white",
+  },
+  "&:hover .arrow": {
+    transform: "rotate(-45deg)",
+  },
+}));
 
-const TilePopup = () => {
-  const [open, setOpen] = useState(false);
+const Arrow = styled("img")({
+  width: "24px",
+  height: "24px",
+  position: "absolute",
+  top: "10px",
+  right: "10px",
+  transform: "rotate(0deg)",
+  transition: "transform 0.3s ease",
+  filter: 'invert(100%)'
+});
 
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
+// Custom styled components for Dialog elements
+const StyledDialog = styled(Dialog)({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "flex-end", // Align the dialog to the bottom of the display
+  overflow: "visible", // Ensure overflow is visible
+  "& .MuiPaper-root": {
+    backgroundColor: "#181818", // Background color of the entire dialog
+    color: "#fff", // Text color
+    height: "70vh", // Adjust this value to control the height of the dialog
+    maxHeight: "100%",
+    position: "relative",
+    overflow: "visible", // Ensure overflow is visible
+    borderRadius: "10px",
+    top: "13vh",
+  },
+});
+
+const CloseButtonContainer = styled("div")({
+  position: "absolute",
+  top: "-50px",
+  right: "50%",
+  transform: "translateX(50%)",
+  zIndex: 1,
+  background: "#181818",
+  borderRadius: "50%",
+});
+
+const StyledDialogContent = styled(DialogContent)({
+  backgroundColor: "#181818", // Background color of the content
+  color: "#fff", // Text color
+  position: "relative",
+  overflow: "visible", // Ensure overflow is visible
+  borderRadius: "10px",
+});
+
+const TilePopup = ({ handleClick }) => {
+  const [open, setOpen] = useState(true);
 
   const handleClose = () => {
     setOpen(false);
+    handleClick();
   };
 
   return (
     <div>
-      <Dialog
+      <StyledDialog
         open={open}
         TransitionComponent={Transition}
         keepMounted
         onClose={handleClose}
         aria-describedby="alert-dialog-slide-description"
+        maxWidth="xl"
+        fullWidth
       >
-        <DialogTitle>{"Tile Details"}</DialogTitle>
-        <DialogContent>
-          {/* Add your images and links here */}
-          <div>
-            <img src="your-image-url.jpg" alt="Example" style={{ width: '100%' }} />
-            <a href="https://example.com">Example Link</a>
-          </div>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color="primary">
-            Close
-          </Button>
-        </DialogActions>
-      </Dialog>
+        <StyledDialogContent>
+          <CloseButtonContainer>
+            <IconButton onClick={handleClose} style={{ color: "white" }}>
+              <CloseIcon />
+            </IconButton>
+          </CloseButtonContainer>
+          <TileContainer>
+            <Tile area="tile1">
+              <Arrow src={arrow} alt="Arrow" className="arrow" />
+              <Typography variant="h6">Tile 1</Typography>
+            </Tile>
+            <Tile area="tile2" style={{ backgroundImage: `url(${Mic})`, backgroundSize: 'cover', backgroundPosition: 'right' }}>
+              <Arrow src={arrow} alt="Arrow" className="arrow" />
+              <Typography variant="h6"
+                fontFamily={"Anton"}
+                color={"white"}
+                fontSize={"30px"}
+                style={{top:'-5vh', left:'-4vw', position:'relative',zIndex:'100'}}>Audition Registration</Typography>
+               <Typography
+                variant="h1"
+                fontFamily={"Helvetica"}
+                color={"white"}
+                fontSize={"10px"}
+                letterSpacing={'1px'}
+                style={{top:'5vh', left:'7vw', position:'relative',fontWeight:'light'}}
+              >
+                Show Us What You Got <br/>And Get To Perform.
+              </Typography>  
+                  </Tile>
+            <Tile area="tile3" style={{ background: "#FFFFFF" }}>
+              <Arrow src={arrow} alt="Arrow" className="arrow" />
+              <Typography
+                variant="h6"
+                fontFamily={"Anton"}
+                color={"black"}
+                fontSize={"30px"}
+                style={{top:'-20%', left:'-15%', position:'relative'}}
+              >
+                Event <br />
+                Gallery
+              </Typography>
+              <Typography
+                variant="h1"
+                fontFamily={"Helvetica"}
+                color={"black"}
+                fontSize={"10px"}
+                letterSpacing={'1px'}
+                style={{top:'20%', left:'-15%', position:'relative',fontWeight:'light'}}
+              >
+                Explore The Photos <br/>And Videos Of The <br/>Event
+              </Typography>
+            </Tile>
+            <Tile area="tile4" style={{ background: "#1E969F" }}>
+              <Arrow src={arrow} alt="Arrow" className="arrow" />
+              <Typography
+                variant="h6"
+                fontFamily={"Anton"}
+                color={"black"}
+                fontSize={"30px"}
+                style={{top:'-20%', left:'-15%', position:'relative'}}
+              >Our <br/> Contact</Typography>
+              <Typography
+                variant="h1"
+                fontFamily={"Helvetica"}
+                color={"black"}
+                fontSize={"10px"}
+                letterSpacing={'1px'}
+                style={{top:'20%', left:'-27%', position:'relative',fontWeight:'light'}}
+              >
+                Having Some <br/> Questions <br/> Specific <br/> To Event ?
+              </Typography>
+            </Tile>
+          </TileContainer>
+        </StyledDialogContent>
+      </StyledDialog>
     </div>
   );
 };
--- client/src/components/Home/Tiles/Tiles.jsx
+++ client/src/components/Home/Tiles/Tiles.jsx
@@ -3,32 +3,59 @@ import './Tiles.scss'
 import arrow from "../../../assets/arrow3x.png"
 import menAvatar from "../../../assets/menAvatar.png"
 import TilePopup from '../Drawer/Drawer'
+
 function Tiles() {
-  const [isClicked,setIsClicked] = useState(false);
+  const [clickedTiles, setClickedTiles] = useState({});
+
+  const handleClick = (tile) => {
+    setClickedTiles(prevState => ({
+      ...prevState,
+      [tile]: !prevState[tile]
+    }));
+  }
+
   return (
     <div className='tileLayout'>
-        <div className="menAvatar">
-            <img src={menAvatar} alt="" />
-        </div>
-    <div className='Tiles'>
-      <div className="one">
-        <div className="title">EVENTS</div>
-        <div className="more">more</div>
-        <div className="arrow"><img src={arrow} alt="arrowIcon" /></div>
-        <TilePopup/>
+      <div className="menAvatar">
+        <img src={menAvatar} alt="Men Avatar" />
       </div>
-      <div className="two"></div>
-      <div className="three"></div>
-      <div className="four"></div>
-      <div className="five"></div>
-      <div className="six"></div>
-      <div className="seven"></div>
-      <div className="eight"></div>
-      <div className="nine"></div>
-      <div className="ten"></div>
+      <div className='Tiles'>
+        <div className="one" onClick={() => handleClick('one')}>
+          <div className="title">EVENTS</div>
+          <div className="more">more</div>
+          <div className="arrow"><img src={arrow} alt="arrowIcon" /></div>
+          {clickedTiles['one'] && <TilePopup color='black' handleClick={() => handleClick('one')} />}
+        </div>
+        <div className="two" onClick={() => handleClick('two')}>
+          {clickedTiles['two'] && <TilePopup color='black' handleClick={() => handleClick('two')} />}
+        </div>
+        <div className="three" onClick={() => handleClick('three')}>
+          {clickedTiles['three'] && <TilePopup color='black' handleClick={() => handleClick('three')} />}
+        </div>
+        <div className="four" onClick={() => handleClick('four')}>
+          {clickedTiles['four'] && <TilePopup color='black' handleClick={() => handleClick('four')} />}
+        </div>
+        <div className="five" onClick={() => handleClick('five')}>
+          {clickedTiles['five'] && <TilePopup color='black' handleClick={() => handleClick('five')} />}
+        </div>
+        <div className="six" onClick={() => handleClick('six')}>
+          {clickedTiles['six'] && <TilePopup color='black' handleClick={() => handleClick('six')} />}
+        </div>
+        <div className="seven" onClick={() => handleClick('seven')}>
+          {clickedTiles['seven'] && <TilePopup color='black' handleClick={() => handleClick('seven')} />}
+        </div>
+        <div className="eight" onClick={() => handleClick('eight')}>
+          {clickedTiles['eight'] && <TilePopup color='black' handleClick={() => handleClick('eight')} />}
+        </div>
+        <div className="nine" onClick={() => handleClick('nine')}>
+          {clickedTiles['nine'] && <TilePopup color='black' handleClick={() => handleClick('nine')} />}
+        </div>
+        <div className="ten" onClick={() => handleClick('ten')}>
+          {clickedTiles['ten'] && <TilePopup color='black' handleClick={() => handleClick('ten')} />}
+        </div>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default Tiles
+export default Tiles;
