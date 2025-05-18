46bafacceb7d0883c1c5332c6e3a3136465f63c0
HrxSrv <128567613+HrxSrv@users.noreply.github.com>
2025-05-18 16:11:04 +0530

    Update axios.js

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
  const currenIp = '192.168.1.3';
 export const makeRequest = axios.create({
-  baseURL: `http://localhost:5000/`, 
+  baseURL: `https://saaz-api.vercel.app/`, 
   withCredentials: false,
 });
 //https://saaz-client-api.onrender.com/
